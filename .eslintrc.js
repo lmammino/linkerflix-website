1f963deb968262fad341b6b6405578256a776dca
Luciano <lucianomammino@gmail.com>
2020-10-19 19:09:42 +0100

    First commit

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+'use strict'
+
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  rules: {
+  },
+  ignorePatterns: ['!.eleventy.js']
+}
---
+++ src/_data/site.js
@@ -0,0 +1,5 @@
+'use strict'
+
+module.exports = {
+  domain: 'linkerflix.com'
+}
